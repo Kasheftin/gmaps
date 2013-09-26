60825e4fe1376bb740a0548ca0a36edf4597e4ad
Kasheftin <Kasheftin@gmail.com>
2013-09-26 18:01:37 +0300

    relative paths corrected

--- js/init.js
+++ js/init.js
@@ -1,10 +1,10 @@
 require.config({
 	paths: {
-		async: "/lib/async-0.1.1",
-		domReady: "/lib/domReady-2.0.1",
-		knockout: "/lib/knockout-2.3.0.min",
-		underscore: "/lib/underscore-1.5.2.min",
-		eventEmitter: "/lib/eventEmitter"
+		async: "../lib/async-0.1.1",
+		domReady: "../lib/domReady-2.0.1",
+		knockout: "../lib/knockout-2.3.0.min",
+		underscore: "../lib/underscore-1.5.2.min",
+		eventEmitter: "../lib/eventEmitter"
 	},
 	shim: {
 		underscore: {
